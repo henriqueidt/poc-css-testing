d91ae81dee10dc7d4eaf4a38fbf26f908dac1713
Henrique Eidt <henriqueidt@gmail.com>
2025-08-12 23:08:02 -0300

    integrate lostpixel

---
+++ lostpixel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  storybookShots: {
+    storybookUrl: "./storybook-static",
+  },
+
+  lostPixelProjectId: "cme9bu05r01kshloi17wz5ttu",
+  apiKey: process.env.LOST_PIXEL_API_KEY,
+};
