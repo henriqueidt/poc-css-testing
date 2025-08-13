a217ee88549e380813ee8e9066d4d944260280d8
Henrique Eidt <henriqueidt@gmail.com>
2025-08-12 22:30:22 -0300

    create userForm stories

---
+++ src/components/UserForm/UserForm.stories.js
@@ -0,0 +1,14 @@
+import UserForm from "./UserForm";
+
+export default {
+  title: "Components/UserForm",
+  component: UserForm,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  args: {},
+};
